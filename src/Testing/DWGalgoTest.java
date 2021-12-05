1404adeeca4e3b30fe67657b5fd773cdf96705a9
Arieh Norton <arieh.norton@gmail.com>
2021-12-05 18:21:40 +0200

    dwgalgo test

---
+++ src/Testing/DWGalgoTest.java
@@ -0,0 +1,52 @@
+package Testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DWGalgoTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getGraph() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void isConnected() {
+    }
+
+    @Test
+    void DFS() {
+    }
+
+    @Test
+    void shortestPathDist() {
+    }
+
+    @Test
+    void shortestPath() {
+    }
+
+    @Test
+    void center() {
+    }
+
+    @Test
+    void tsp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void load() {
+    }
+}
