0c9e5e886c79991c06579f0cf7c14822cc30e2b6
Arieh Norton <arieh.norton@gmail.com>
2021-12-06 20:02:20 +0200

    save file

--- src/Ex2/DWGalgo.java
+++ src/Ex2/DWGalgo.java
@@ -1,17 +1,15 @@
 package Ex2;
 
-import api.DirectedWeightedGraph;
-import api.DirectedWeightedGraphAlgorithms;
-import api.EdgeData;
-import api.NodeData;
+import api.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class DWGalgo implements DirectedWeightedGraphAlgorithms {
     DirectedWeightedGraph graph;
-
 
 
     @Override
@@ -29,12 +27,12 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
     public DirectedWeightedGraph copy() {
         DirectedWeightedGraph copy_graph = new DWG();
         Iterator<NodeData> itr_node = this.graph.nodeIter();
-        while(itr_node.hasNext()) {
+        while (itr_node.hasNext()) {
             NodeData temp_node = new GNode(itr_node.next());
             copy_graph.addNode(temp_node);
         }
         Iterator<EdgeData> itr_edge = this.graph.edgeIter();
-        while(itr_edge.hasNext()) {
+        while (itr_edge.hasNext()) {
             EdgeData temp_edge = new Edge(itr_edge.next());
             copy_graph.connect(temp_edge.getSrc(), temp_edge.getDest(), temp_edge.getWeight());
         }
@@ -47,36 +45,33 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
         // testing leead commit is gay
         int nodes = 0;
         Iterator<NodeData> itr_node = this.graph.nodeIter();
-        while(itr_node.hasNext()){
+        while (itr_node.hasNext()) {
             itr_node.next();
             nodes++;
         }
-        for(int i = 0; i< nodes; i++){
+        for (int i = 0; i < nodes; i++) {
             boolean[] visited = new boolean[nodes];
             DFS(this.graph, i, visited);
-            for (boolean b: visited)
-            {
+            for (boolean b : visited) {
                 if (!b) {
                     return false;
                 }
             }
         }
         return true;
-        }
+    }
 
-        // Function to perform DFS traversal on the graph on a graph
-        public static void DFS(DirectedWeightedGraph graph, int v, boolean[] visited) {
-            visited[v] = true;
-            Iterator<EdgeData> itr_edge = graph.edgeIter(v);
-            while (itr_edge.hasNext()) {
-                EdgeData temp_edge = new Edge(itr_edge.next());
-                if (!visited[temp_edge.getDest()]) {
-                    DFS(graph, temp_edge.getDest(), visited);
-                }
+    // Function to perform DFS traversal on the graph on a graph
+    public static void DFS(DirectedWeightedGraph graph, int v, boolean[] visited) {
+        visited[v] = true;
+        Iterator<EdgeData> itr_edge = graph.edgeIter(v);
+        while (itr_edge.hasNext()) {
+            EdgeData temp_edge = new Edge(itr_edge.next());
+            if (!visited[temp_edge.getDest()]) {
+                DFS(graph, temp_edge.getDest(), visited);
             }
         }
-
-
+    }
 
 
     @Override
@@ -84,8 +79,10 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
         return find_shortestPathDist(src, dest, this.graph);
     }
 
-    /** This function receives the src, dest and graph. It returns the shortest path between src and dest nodes.
-     * Return: Double. */
+    /**
+     * This function receives the src, dest and graph. It returns the shortest path between src and dest nodes.
+     * Return: Double.
+     */
     private double find_shortestPathDist(int src, int dest, DirectedWeightedGraph graph) {
         //finding the highest Node ID.
         int i, j;
@@ -103,27 +100,25 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
         double[][] node_matrix = new double[max_id][max_id];
         for (i = 0; i < max_id; i++) {
             for (j = 0; j < max_id; j++) {
-                try{
-                    if(graph.getNode(i)==null || graph.getNode(j)==null){
+                try {
+                    if (graph.getNode(i) == null || graph.getNode(j) == null) {
                         node_matrix[i][j] = -1.0;
                         continue;
                     }
-                }
-                catch (NullPointerException e){
+                } catch (NullPointerException e) {
                     node_matrix[i][j] = -1.0;
                     continue;
                 }
 
                 //initializing the indexes that edges don't exist with MAX:
                 try {
-                    if (graph.getEdge(i, j) == null && graph.getNode(i)!=null && graph.getNode(j)!=null ) {
+                    if (graph.getEdge(i, j) == null && graph.getNode(i) != null && graph.getNode(j) != null) {
                         node_matrix[i][j] = 0.0;
                         continue;
                     }
-                }
-                catch (NullPointerException d){
-                        node_matrix[i][j] = 0.0;
-                        continue;
+                } catch (NullPointerException d) {
+                    node_matrix[i][j] = 0.0;
+                    continue;
                 }
 
                 //initializing the main diagonal with zeros:
@@ -135,18 +130,19 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
                 node_matrix[i][j] = graph.getEdge(i, j).getWeight();
             }
         }
-        System.out.println("Starting array: "+Arrays.deepToString(node_matrix));
         return warshall(src, dest, node_matrix);
     }
 
-    /** This is the Floyed Warshall Algorith, function. It calculates the shortest path between two indexes.
-     * Return: Double. */
+    /**
+     * This is the Floyed Warshall Algorith, function. It calculates the shortest path between two indexes.
+     * Return: Double.
+     */
     private double warshall(int src, int dest, double[][] node_matrix) {
         int len = node_matrix.length;
-        for(int k=0; k<len; k++)
-            for(int i=0; i<len; i++)
-                for(int j=0; j<len; j++) {
-                    if(node_matrix[i][j] == -1.0)
+        for (int k = 0; k < len; k++)
+            for (int i = 0; i < len; i++)
+                for (int j = 0; j < len; j++) {
+                    if (node_matrix[i][j] == -1.0)
                         continue;
                     if (node_matrix[i][k] > 0.0 && node_matrix[k][j] > 0.0) {
                         if (node_matrix[i][j] == 0.0 && i != j) {
@@ -157,34 +153,166 @@ public class DWGalgo implements DirectedWeightedGraphAlgorithms {
                             node_matrix[i][j] = node_matrix[i][k] + node_matrix[k][j];
                     }
                 }
-
-        System.out.println("Finish array: "+Arrays.deepToString(node_matrix));
+        System.out.println(node_matrix[src][dest]);
+        System.out.println(Arrays.deepToString(node_matrix));
         return node_matrix[src][dest];
     }
 
 
     @Override
-    public List<NodeData> shortestPath ( int src, int dest){
+    public List<NodeData> shortestPath(int src, int dest) {
+        List<NodeData> nodePath = new ArrayList<>();
+
+        //finding the highest Node ID.
+        int i, j;
+        int max_id = 0;
+        Iterator<NodeData> itr = graph.nodeIter();
+        while (itr.hasNext()) {
+            NodeData temp = itr.next();
+            if (temp.getKey() > max_id)
+                max_id = temp.getKey();
+        }
+        max_id += 1;
+
+        //initializing the matrix:
+        Iterator<NodeData> itr2 = graph.nodeIter();
+        double[][] node_matrix = new double[max_id][max_id];
+        for (i = 0; i < max_id; i++) {
+            for (j = 0; j < max_id; j++) {
+                try {
+                    if (graph.getNode(i) == null || graph.getNode(j) == null) {
+                        node_matrix[i][j] = -1.0;
+                        continue;
+                    }
+                } catch (NullPointerException e) {
+                    node_matrix[i][j] = -1.0;
+                    continue;
+                }
+
+                //initializing the indexes that edges don't exist with MAX:
+                try {
+                    if (graph.getEdge(i, j) == null && graph.getNode(i) != null && graph.getNode(j) != null) {
+                        node_matrix[i][j] = 0.0;
+                        continue;
+                    }
+                } catch (NullPointerException d) {
+                    node_matrix[i][j] = 0.0;
+                    continue;
+                }
+
+                //initializing the main diagonal with zeros:
+                if (i == j) { // same node.
+                    node_matrix[i][j] = 0.0;
+                    continue;
+                }
+                //else the edge exists,  initialize the index with its weight.
+                node_matrix[i][j] = graph.getEdge(i, j).getWeight();
+            }
+        }
+        return warshallNodePath(src, dest, node_matrix, nodePath);
+    }
+
+    /**
+     * This is the Floyed Warshall Algorith, function. It calculates the shortest path between two indexes.
+     * Return: Double.
+     */
+    private List<NodeData> warshallNodePath(int src, int dest, double[][] node_matrix, List<NodeData> list) {
+        List<NodeData> final_list = new ArrayList<>();
+        int len = node_matrix.length;
+        for (int k = 0; k < len; k++)
+            for (int i = 0; i < len; i++)
+                for (int j = 0; j < len; j++) {
+                    if (node_matrix[i][j] == -1.0)
+                        continue;
+                    if (node_matrix[i][k] > 0.0 && node_matrix[k][j] > 0.0) {
+                        if (node_matrix[i][j] == 0.0 && i != j) {
+                            node_matrix[i][j] = node_matrix[i][k] + node_matrix[k][j];
+                            if (i == src || j == dest) {
+                                list.add(this.graph.getNode(k));
+                                list.add(this.graph.getNode(j));
+                            }
+                            continue;
+                        }
+                        if (node_matrix[i][k] + node_matrix[k][j] < node_matrix[i][j]) {
+                            node_matrix[i][j] = node_matrix[i][k] + node_matrix[k][j];
+                            if (i == src || j == dest) {
+//                                list.clear();
+                                list.add(this.graph.getNode(k));
+                                list.add(this.graph.getNode(j));
+                            }
+                        }
+                    }
+                }
+//        if (list.isEmpty()){
+//                list.add(this.graph.getNode(src));
+//                list.add(this.graph.getNode(dest));
+//            }
+//        else{
+//            list.add(0,this.graph.getNode(src));
+//        }
+        System.out.println(Arrays.deepToString(node_matrix));
+        for (NodeData gn : list) {
+            if (!final_list.contains(gn)) {
+                final_list.add(gn);
+            }
+        }
+        final_list.add(0, this.graph.getNode(src));
+        return final_list;
+    }
+
+
+    @Override
+    public NodeData center() {
         return null;
     }
 
     @Override
-    public NodeData center () {
+    public List<NodeData> tsp(List<NodeData> cities) {
         return null;
     }
 
     @Override
-    public List<NodeData> tsp (List < NodeData > cities) {
-        return null;
+    public boolean save(String file) {
+        JSONObject top = new JSONObject();
+        JSONArray edge_arr = new JSONArray();
+        top.put("Edges", edge_arr);
+        JSONArray node_arr = new JSONArray();
+        top.put("Nodes", node_arr);
+        Iterator <EdgeData> itr_edge = this.graph.edgeIter();
+        while(itr_edge.hasNext()){
+            EdgeData temp = itr_edge.next();
+            JSONObject edge_obj = new JSONObject();
+            edge_obj.put("src", temp.getSrc());
+            edge_obj.put("w" , temp.getWeight());
+            edge_obj.put("dest", temp.getDest());
+            edge_arr.add(edge_obj);
+        }
+        Iterator<NodeData> itr_nodes = this.graph.nodeIter();
+        while (itr_nodes.hasNext()){
+            NodeData temp = itr_nodes.next();
+            JSONObject node_obj = new JSONObject();
+            node_obj.put("pos", temp.getLocation().x() + "," + temp.getLocation().y()+ "," + temp.getLocation().z());
+            node_obj.put("id" , temp.getKey());
+            edge_arr.add(node_obj);
+        }
+        try (FileWriter f = new FileWriter(file)) {
+            //We can write any JSONArray or JSONObject instance to the file
+            f.write(top.toJSONString());
+            f.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
+
     @Override
-    public boolean save (String file){
+    public boolean load(String file) {
         return false;
     }
 
-    @Override
-    public boolean load (String file){
-        return false;
-    }
 }
+
+
